d103fe71300f54eb7c5cce3e9fd2cc033a5ca02c
Eric Sohn <esohn@Erics-MacBook-Pro-3.local>
2019-02-24 20:16:08 -0500

    Able to access API. Able to fetch list of wallpapers and random wallpaper

--- project-two/src/App.js
+++ project-two/src/App.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
 import { Route, Link } from "react-router-dom";
-import { wallpaper } from './services/wallpaper';
+import { fetchWallpaper, fetchRandomWallpaper } from './services/wallpaper';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-
+      wallpaper: {}
     }
+  }
+
+  async getWallpapers() {
+    const wallpaper = await fetchWallpaper();
+    this.setState({
+      wallpaper: wallpaper
+    })
+  }
+
+  async getRandom() {
+    const random = await fetchRandomWallpaper();
+    this.setState({
+      wallpaper: random
+    })
+  }
+
+  componentDidMount() {
+    this.getWallpapers();
+    this.getRandom();
   }
 
   render() {
--- project-two/src/services/wallpaper.js
+++ project-two/src/services/wallpaper.js
@@ -1,10 +1,18 @@
 import axios from 'axios';
 
-const BASE_URL = '';
+const TOKEN = process.env.REACT_APP_API_KEY;
+const BASE_URL = 'https://api.pexels.com/v1';
+
+const api = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+      'Authorization': `Bearer ${TOKEN}`
+  }
+});
 
 const fetchWallpaper = async () => {
   try {
-    const resp = await axios(`${BASE_URL}`)
+    const resp = await api(`search?query=wallpaper&per_page=15&page=1`)
     return resp.data;
   } catch(e) {
     console.log('got a problem: ', e);
@@ -12,4 +20,17 @@ const fetchWallpaper = async () => {
   }
 }
 
-export { wallpaper };
+const fetchRandomWallpaper = async () => {
+  try {
+    const resp = await api(`curated?per_page=1&page=1`)
+    return resp.data;
+  } catch(e) {
+    console.log('got a problem: ', e);
+    return [];
+  }
+}
+
+export {
+  fetchWallpaper,
+  fetchRandomWallpaper
+};
